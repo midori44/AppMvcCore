08514c58807e6e01fd05818f463531b984aaee97
midori44 <midoyasu@hotmail.co.jp>
2018-03-30 17:22:52 +0900

    プロジェクト ファイルを追加します。

---
+++ AppMvcCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppMvcCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
